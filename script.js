35190eebbbdffcf2b3dc360bef8b1f55bdfe5c6b
aiheadnotes <kasperknaap@hotmail.com>
2025-05-03 21:43:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contact form submission (just an example)
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+  });
+  
